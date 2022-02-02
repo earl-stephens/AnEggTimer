c8f875c7694fee5b48392333dd434f749dd2ef54
earl-stephens <stephensearl@hotmail.com>
2022-02-01 23:53:55 -0700

    Setup the abstract class timer

---
+++ src/application/DisplayCounter.java
@@ -0,0 +1,13 @@
+package application;
+
+import java.util.TimerTask;
+
+public class DisplayCounter extends TimerTask {
+
+	@Override
+	public void run() {
+		System.out.println("The timer is running.");
+		
+	}
+
+}
